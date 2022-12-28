6bc8ebca7506bb8b06419a9ac03e7704ce8ea1e7
NitinRana-code <nitinranasd@gmail.com>
2022-12-28 20:57:29 +0530

    updated files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.8
 COPY . .
+RUN python -m pip install --upgrade pip
 RUN pip install python-telegram-bot
+RUN pip install openai telepot
 
 CMD python main.py
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from telegram.ext.updater import Updater
 from telegram.ext import ConversationHandler,CommandHandler,RegexHandler 
-import telegram.ext
-import os
 import openai
 import telepot
 from Cred import tokens 
@@ -9,21 +7,20 @@ key = tokens.TOKEN_OPENAI
 openai.api_key = key
 
 token = tokens.TOKEN_TELGERAM
+bot = telepot.Bot(token)  
 
 updater = Updater(token, use_context=True  )
 dispatcher = updater.dispatcher
 
 def start(update,context):
-    update.message.reply_text("""Welcome!!!🌟✨✨
+    update.message.reply_text("""Welcome!!!🌟✨🎆
 Your bot 🤖 is ready to use :)
 Try these commands👇
 /say --> ask me anything!🤔
-/help --> if you want any help🙋‍♂️
-/img --> to generate image🐈""")
-    
-    
-    
-    
+/img --> to generate image🐈
+/clear --> clear the conversation🗣️
+/help --> if you want any help🙋‍♂️""")
+
 query = []
 res = ""
 pr =""
@@ -32,32 +29,35 @@ def say(update, context):
     global pr
     global res
     inp_msg = update.message.text
-    inp_args = inp_msg.split("/say ")     
+    inp_args = inp_msg.split("/say ")   #query message
+    inp_msg_token = inp_msg.split("-t")     #max_token value
 
     if len(inp_args)>1:  #if no message typed by user
         query.append(inp_args[1])
         pr+=f"Human: {query[len(query)-1]}. \nAi:"
         
+        if(len(inp_msg_token)>1):
+            inp_token = int(inp_msg_token[1])
+        else: inp_token = 150
+
         response = openai.Completion.create(
         model="text-davinci-003",
         prompt=pr,
         temperature=0.9,
-        max_tokens=150,
+        max_tokens=inp_token,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
         stop=[" Human:", " AI:"]
         )
-        
+               
         msg = response["choices"][0]["text"]
         res = msg
         update.message.reply_text(msg)
         pr+=f" {res}\n"
-        
     else:
         update.message.reply_text("/say ask me something")
 
-bot = telepot.Bot(token)  
 def img(update, context):
     chat_id = update.message.chat_id
     inp_msg = update.message.text
@@ -75,17 +75,26 @@ def img(update, context):
     else:
         update.message.reply_text("/img type your prompt to generate an image")
 
-
 def help(update, context):
     update.message.reply_text("""Try these following command to start with us ⬇️
-/start --> Start with us!🚀
-/say type any thing you want🤔
-/img type prompt to generate image🐈""")
 
+/start --> Start with us!🚀
+/say type any thing you want use -t flag for length of tokens (default is 150)🤔
+/img type prompt to generate image🐈
+/clear --> To reset the conversation""")
+
+def clear(update, containes):   #clear the conversation
+    update.message.reply_text("Conversations are cleared!!!")
+    global pr
+    pr = ""
+
+# Handling the events
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("say", say))
 dispatcher.add_handler(CommandHandler("img", img))
 dispatcher.add_handler(CommandHandler("help", help))
+dispatcher.add_handler(CommandHandler("clear", clear))
+
 updater.start_polling()
 updater.idle()
 
